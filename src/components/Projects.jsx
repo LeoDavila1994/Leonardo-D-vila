bcb5397ec6ed8f74e5de1b56c95959b99c081176
LeoDavila1994 <liodavila.ld@gmail.com>
2022-08-14 21:38:34 -0500

    imgs

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,22 +14,22 @@ const Projects = () => {
             </div>
             <div className="grid">
                 <div className="projects-img-container img-1">
-                    <a href="https://multiverserickandmorty.netlify.app/"><img src={ec} className="projects-img" /></a>
+                    <a href="https://sunnyshop.netlify.app/"><img src={ec} className="projects-img" /></a>
                 </div>
                 <div className="projects-img-container img-2">
-                    <a href="https://sunnyshop.netlify.app/"><img src={crud} className="projects-img"/></a>
+                    <a href="https://crudtousers.netlify.app/"><img src={crud} className="projects-img"/></a>
                 </div>
                 <div className="projects-img-container img-3">
-                    <a href="https://landingpagefmh.netlify.app/"><img src={wa} className="projects-img"/></a>
+                    <a href="https://weatherappnow.netlify.app/"><img src={wa} className="projects-img"/></a>
                 </div>
                 <div className="projects-img-container img-4">
                     <a href="https://pokedextotrainers.netlify.app/"><img src={pkdx} className="projects-img" /></a>
                 </div>
                 <div className="projects-img-container img-5">
-                    <a href="https://weatherappnow.netlify.app/"><img src={rym} className="projects-img"/></a>
+                    <a href="https://multiverserickandmorty.netlify.app/"><img src={rym} className="projects-img"/></a>
                 </div>
                 <div className="projects-img-container img-6">
-                    <a href="https://crudtousers.netlify.app/"><img src={lp} className="projects-img"/></a>
+                    <a href="https://landingpagefmh.netlify.app/"><img src={lp} className="projects-img"/></a>
                 </div>
             </div>
         </div>
