102fac7f555b8fe78f33b2e657d5b2a45c3d7d16
LeoDavila1994 <liodavila.ld@gmail.com>
2022-08-14 23:28:04 -0500

    loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Loader = () => {
+    return (
+        <div className='loader-container'>
+            <p className='w'>W</p>
+            <p className='e1'>E</p>
+            <p className='l'>L</p>
+            <p className='c'>C</p>
+            <p className='o'>O</p>
+            <p className='m'>M</p>
+            <p className='e2'>E</p>
+        </div>
+    );
+};
+
+export default Loader;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
-import { useNavigate} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import Loader from './Loader';
 
 const NavBar = () => {
+
+    useEffect(() => {
+
+        const loading = () =>{
+            setIsLoading(false)
+        }
+        setTimeout(loading, 2000)
+    },[]);
+
+    const [isLoading, setIsLoading] = useState(true);
 
     const navigate = useNavigate();
 
@@ -28,20 +39,25 @@ const NavBar = () => {
 
     return (
         <>
-            <input type="checkbox" id='navbar'/>
-            <div className='navbar-container'>
-                <label className='icon-container' htmlFor='navbar'>
-                    <div className='icon-1'></div>
-                    <div className="icon-2"></div>
-                </label>
-            </div>
-            <div className='menu'>
-                <label htmlFor="navbar"><p onClick={home}>Home</p></label>
-                <label htmlFor="navbar"><p onClick={about}>About</p></label>
-                <label htmlFor="navbar"><p onClick={skills}>Skills</p></label>
-                <label htmlFor="navbar"><p onClick={projects}>Projects</p></label>
-                <label htmlFor="navbar"><p onClick={contact}>Contact</p></label>
-            </div>
+            {isLoading ? <Loader /> : (
+                <>
+                    <input type="checkbox" id='navbar' />
+                    <div className='navbar-container'>
+                        <label className='icon-container' htmlFor='navbar'>
+                            <div className='icon-1'></div>
+                            <div className="icon-2"></div>
+                        </label>
+                    </div>
+                    <div className='menu'>
+                        <label htmlFor="navbar"><p onClick={home}>Home</p></label>
+                        <label htmlFor="navbar"><p onClick={about}>About</p></label>
+                        <label htmlFor="navbar"><p onClick={skills}>Skills</p></label>
+                        <label htmlFor="navbar"><p onClick={projects}>Projects</p></label>
+                        <label htmlFor="navbar"><p onClick={contact}>Contact</p></label>
+                    </div>
+                </>
+            )}
+
         </>
     );
 };
