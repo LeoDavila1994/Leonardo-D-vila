0a6be0ccdfbc75fc402438c46c5338d89f5fa28d
LeoDavila1994 <liodavila.ld@gmail.com>
2022-08-16 21:02:49 -0500

    navcolor

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,27 +13,57 @@ const NavBar = () => {
     },[]);
 
     const [isLoading, setIsLoading] = useState(true);
+    const [ homeColor, setTextColor ] = useState(true);
+    const [ aboutColor, setAboutColor ] = useState(false);
+    const [ skillsColor, setSkillsColor ] = useState(false);
+    const [ projectsColor, setProjectsColor ] = useState(false);
+    const [ contactColor, setContactColor ] = useState(false);
 
     const navigate = useNavigate();
 
     const home = () => {
         navigate("/");
+        setTextColor(true);
+        setAboutColor(false);
+        setSkillsColor(false);
+        setProjectsColor(false);
+        setContactColor(false);
     }
 
     const about = () => {
         navigate("/about");
+        setTextColor(false);
+        setAboutColor(true);
+        setSkillsColor(false);
+        setProjectsColor(false);
+        setContactColor(false);
     }
 
     const skills = () => {
         navigate("/skills");
+        setTextColor(false);
+        setAboutColor(false);
+        setSkillsColor(true);
+        setProjectsColor(false);
+        setContactColor(false);
     }
 
     const projects = () => {
         navigate("/projects");
+        setTextColor(false);
+        setAboutColor(false);
+        setSkillsColor(false);
+        setProjectsColor(true);
+        setContactColor(false);
     }
 
     const contact = () => {
         navigate("/contact");
+        setTextColor(false);
+        setAboutColor(false);
+        setSkillsColor(false);
+        setProjectsColor(false);
+        setContactColor(true);
     }
 
 
@@ -49,11 +79,11 @@ const NavBar = () => {
                         </label>
                     </div>
                     <div className='menu'>
-                        <label htmlFor="navbar"><p onClick={home}>Inicio</p></label>
-                        <label htmlFor="navbar"><p onClick={about}>Sobre mi</p></label>
-                        <label htmlFor="navbar"><p onClick={skills}>Habilidades</p></label>
-                        <label htmlFor="navbar"><p onClick={projects}>Proyectos</p></label>
-                        <label htmlFor="navbar"><p onClick={contact}>Contacto</p></label>
+                        <label htmlFor="navbar"><p onClick={home} style={{color: `${homeColor? "salmon": "white"}`}}>Inicio</p></label>
+                        <label htmlFor="navbar"><p onClick={about} style={{color: `${aboutColor? "salmon": "white"}`}}>Sobre mi</p></label>
+                        <label htmlFor="navbar"><p onClick={skills} style={{color: `${skillsColor? "salmon": "white"}`}}>Habilidades</p></label>
+                        <label htmlFor="navbar"><p onClick={projects} style={{color: `${projectsColor? "salmon": "white"}`}}>Proyectos</p></label>
+                        <label htmlFor="navbar"><p onClick={contact} style={{color: `${contactColor? "salmon": "white"}`}}>Contacto</p></label>
                     </div>
                 </>
             )}
