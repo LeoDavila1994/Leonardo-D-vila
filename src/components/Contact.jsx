3ff60eafac07620f2b05af7e5dfccc3c0bc5e194
LeoDavila1994 <liodavila.ld@gmail.com>
2022-08-19 17:45:21 -0500

    form funcional

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,40 +1,46 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import emailjs from '@emailjs/browser';
+import React, { useRef } from 'react';
+
 
 const Contact = () => {
 
     const swich = useSelector(state => state.swich);
 
-    const [ name, setName ] = useState("");
-    const [ email, setEmail ] = useState("");
-    const [ mensaje, setMensaje ] = useState("");
+    const form = useRef();
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [mensaje, setMensaje] = useState("");
 
     const submit = e => {
         e.preventDefault();
-        alert("Enviar contacto");
-        setName("");
-        setEmail("");
-        setMensaje("");
+
+        emailjs.sendForm("service_1jcabhh", "template_fqbxd3b", e.target, "WBQD4QTr00f6A6D7F")
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
     }
 
+
     return (
-        <div className='contact-container' style={{backgroundColor: `${swich? "#414152":"#f5f5f7"}`}}>
+        <div className='contact-container' style={{ backgroundColor: `${swich ? "#414152" : "#f5f5f7"}` }}>
             <div className="contact-card">
                 <div className='contact-title-container'>
-                    <h2 style={{color: `${swich? "#f5f5f7":"#313132"}`}}>CONTACTO</h2>
+                    <h2 style={{ color: `${swich ? "#f5f5f7" : "#313132"}` }}>CONTACTO</h2>
                 </div>
                 <div className='contact-text-container'>
-                    <p style={{color: `${swich? "#f5f5f7":"#313132"}`}}>¿Tienes algo en mente?</p>
-                    <p style={{color: `${swich? "#f5f5f7":"#313132"}`}}>Bueno, ¡Hagamoslo realidad!</p>
+                    <p style={{ color: `${swich ? "#f5f5f7" : "#313132"}` }}>¿Tienes algo en mente?</p>
+                    <p style={{ color: `${swich ? "#f5f5f7" : "#313132"}` }}>Bueno, ¡Hagamoslo realidad!</p>
                 </div>
-                <form className='form' onSubmit={submit} name="contacto" netlify="">
+                <form className='form' onSubmit={submit} ref={form}>
                     <div className='form-container'>
-                        <p>Nombre:</p>
-                        <input type="text" className='form-input' value={name} onChange={e => setName(e.target.value)}/>
-                        <p>Email:</p>
-                        <input type="email"className='form-input' value={email} onChange={e => setEmail(e.target.value)}/>
-                        <p>Mensaje:</p>
-                        <textarea className='form-area' value={mensaje} onChange={e => setMensaje(e.target.value)}></textarea>
+                        <label htmlFor='name'>Nombre:</label>
+                        <input type="text" id='name' className='form-input' value={name} onChange={e => setName(e.target.value)} name="user-name"/>
+                        <label htmlFor='email'>Email:</label>
+                        <input type="email" id='email' className='form-input' value={email} onChange={e => setEmail(e.target.value)} name="user-email"/>
+                        <label htmlFor='mensaje'>Mensaje:</label>
+                        <textarea className='form-area' id='mensaje' value={mensaje} onChange={e => setMensaje(e.target.value)} name="user-mensaje"></textarea>
                     </div>
                     <button className='btn-form'>Enviar</button>
                 </form>
