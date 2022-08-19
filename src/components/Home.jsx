254a2eef70c35bcdc258979b710d4b7cc3d23ea3
LeoDavila1994 <liodavila.ld@gmail.com>
2022-08-19 18:57:18 -0500

    cv download

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,19 +6,22 @@ const Home = () => {
     const swich = useSelector(state => state.swich);
 
     return (
-        <div className='home-container' style={{backgroundColor: `${swich? "#414152":"#f5f5f7"}`}}>
-            <div className="home-card-container" style={{backgroundColor: `${swich? "#6e6e73":"white"}`}}>
+        <div className='home-container' style={{ backgroundColor: `${swich ? "#414152" : "#f5f5f7"}` }}>
+            <div className="home-card-container" style={{ backgroundColor: `${swich ? "#6e6e73" : "white"}` }}>
                 <div className="home-front-page"></div>
                 <div className="home-img-container"></div>
                 <div className='home-text-container'>
-                    <h1 style={{color: `${swich? "#f5f5f7":"#313132"}`}}>Hola soy Leo</h1>
-                    <p style={{color: `${swich? "pink":"salmon"}`}}>Desarrollador Frontend</p>
+                    <h1 style={{ color: `${swich ? "#f5f5f7" : "#313132"}` }}>Hola soy Leo</h1>
+                    <p style={{ color: `${swich ? "pink" : "salmon"}` }}>Desarrollador Frontend</p>
+                    <div className='btn-resumen'>
+                        <a href="src\pdf\cv.pdf" target="_blank" download="Leonardo Dávila CV"><i className="fa-solid fa-file"></i></a>
+                    </div>
                 </div>
                 <div className="home-icons-container">
-                <a href="https://wa.me/524495921411" style={{color: `${swich? "pink":"salmon"}`}}><i className="fa-brands fa-square-whatsapp"></i></a>
-                    <a href="https://www.linkedin.com/in/leodavila/" style={{color: `${swich? "pink":"salmon"}`}}><i className="fa-brands fa-linkedin"></i></a>
-                    <a href="https://www.facebook.com/profile.php?id=1783522575" style={{color: `${swich? "pink":"salmon"}`}}><i className="fa-brands fa-square-facebook"></i></a>
-                    <a href="https://www.instagram.com/liodavila1/" style={{color: `${swich? "pink":"salmon"}`}}><i className="fa-brands fa-square-instagram"></i></a>
+                    <a href="https://wa.me/524495921411" style={{ color: `${swich ? "pink" : "salmon"}` }}><i className="fa-brands fa-square-whatsapp"></i></a>
+                    <a href="https://www.linkedin.com/in/leodavila/" style={{ color: `${swich ? "pink" : "salmon"}` }}><i className="fa-brands fa-linkedin"></i></a>
+                    <a href="https://www.facebook.com/profile.php?id=1783522575" style={{ color: `${swich ? "pink" : "salmon"}` }}><i className="fa-brands fa-square-facebook"></i></a>
+                    <a href="https://www.instagram.com/liodavila1/" style={{ color: `${swich ? "pink" : "salmon"}` }}><i className="fa-brands fa-square-instagram"></i></a>
                 </div>
             </div>
         </div>
